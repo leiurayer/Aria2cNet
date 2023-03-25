1bf8b0471fe1df7ba8ce53abc070d0868f549fe1
leiurayer <1432593898@qq.com>
2023-03-25 14:18:24 +0800

    添加项目文件。

---
+++ Aria2cNet/Client/Models/AriaChangeUri.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Aria2cNet.Client.Models
+{
+    [JsonObject]
+    public class AriaChangeUri
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("jsonrpc")]
+        public string Jsonrpc { get; set; }
+
+        [JsonProperty("result")]
+        public List<int> Result { get; set; }
+
+        [JsonProperty("error")]
+        public AriaError Error { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ Aria2cNet/Client/Models/SystemMulticallMathod.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Aria2cNet.Client.Models
+{
+    [JsonObject]
+    public class SystemMulticallMathod
+    {
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("params")]
+        public List<object> Params { get; set; }
+    }
+}
